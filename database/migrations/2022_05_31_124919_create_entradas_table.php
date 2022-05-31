aec39c811da95995c6303b05f07e68b303f646ff
MakaGrimhilde <simtoclatro@gmail.com>
2022-05-31 17:33:34 +0200

    Segundo commit del proyecto. Añadidos los ficheros de migraciones para la creación de las tablas en la base de datos

---
+++ database/migrations/2022_05_31_124919_create_entradas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntradasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entradas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->constrained('usuarios');
+            $table->foreignId('categoria_id')->constrained('categorias');
+            $table->string('titulo');
+            $table->string('imagen');
+            $table->text('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entradas');
+    }
+}
